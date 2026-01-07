5e28b02ebae0bfdc637a205c895941414dc34035
ShlomiAlg <algshlomi@gmail.com>
2026-01-07 22:08:38 +0200

    0701Pb

--- src/main/java/com/forces/repository/DeadZoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.forces.repository;
-
-import com.forces.model.DeadZone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeadZoneRepository extends JpaRepository<DeadZone, String> {
-}
